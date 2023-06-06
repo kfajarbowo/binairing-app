c1732667dd2c2ebb182207bd8b3fed2ee1ad69c1
Kidung Fajar Prabowo <kfajarbowo12@gmail.com>
2023-06-06 22:12:15 +0700

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Home() {
+	const test = {
+		color: 'green',
+	};
+	return (
+		<Container style={test}>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, vero
+			ipsa delectus quam maiores aliquam aut sunt at qui perspiciatis facere
+			tempora recusandae nemo velit ex nam eius laboriosam. Velit?
+		</Container>
+	);
+}
+
+export default Home;
