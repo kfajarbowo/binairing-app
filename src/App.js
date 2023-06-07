f6c2a636166aeb493ac8926bfb6fc2c7d1dc3b2c
Kidung Fajar Prabowo <kfajarbowo12@gmail.com>
2023-06-07 13:15:06 +0700

    code : clean up code (#2)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 // import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
