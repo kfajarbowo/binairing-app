0ea3486ee688a3f43cdd179c5ff1f864c29bb070
FajriAulia471 <124769986+FajriAulia471@users.noreply.github.com>
2023-06-07 12:42:25 +0700

    home v.1.0.0 (#1)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 // import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
