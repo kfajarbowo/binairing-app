96874a58f041f0640e2e854943830fe78de8c2af
meilanyfrisilliamarbun <134934745+meilanyfrisilliamarbun@users.noreply.github.com>
2023-06-08 14:46:27 +0700

    add search (#5)
    Co-authored-by: meilanyfrisillia <meilanyfrisillia02@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register'
 import ResetPassword from './pages/ResetPassword'
+import Search from './pages/Search';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/resetpassword" element={<ResetPassword />} />
+				<Route path="/search" element={<Search />} />
 			</Routes>
 		</BrowserRouter>
 	);
