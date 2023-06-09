eca57009487c46d2310d6fc4e144c41f2916c32a
FajriAulia471 <fajriauliarachmandha@gmail.com>
2023-06-09 19:53:00 +0700

    feat: ngepisahin css untuk home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
