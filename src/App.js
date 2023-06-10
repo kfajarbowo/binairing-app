0e0822f02f56c0be80f3c5b2df1af0c21442d6be
FajriAulia471 <124769986+FajriAulia471@users.noreply.github.com>
2023-06-10 08:47:22 +0700

    Fajri feature (#8)
    * feat: interative home, header belum selesai
    
    * feat: ngepisahin css untuk home
    
    * feat: Home
    
    * fix header
    
    * fix css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 // import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register'
-import ResetPassword from './pages/ResetPassword'
-import Search from './pages/Search';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ResetPassword from "./pages/ResetPassword";
+import Search from "./pages/Search";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/resetpassword" element={<ResetPassword />} />
-				<Route path="/search" element={<Search />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
